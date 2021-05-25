1bd2fe821f98805930e529b493ac1c3f473190c2
Xin Zhang <xinzhang@Jolies-MacBook-Pro-2.local>
2021-05-24 23:58:25 -0700

    Initial try on github api.
    1. Setup material ui framework.
    2. Use material ui autocomplete to help with search suggestions.
    3. Add axios to handle github rest apis.
    4. Play with debounce suggestions and functional component needs
    to use callback with debounce.
    
    TODO:
    1. Still see api call loops. Need to resolve that.
    2. Refactor out search component.
    3. Build search result.
    
    --xzhang

--- src/App.js
+++ src/App.js
@@ -1,5 +1,90 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { Container, Box, TextField, CircularProgress } from "@material-ui/core";
+import { Autocomplete } from "@material-ui/lab";
+import axios from "axios";
+
+import { debounce } from "./utils/helper";
+
 function App() {
-  return <div>Hello World</div>;
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const [searchValue, setSearchValue] = useState("");
+  const [suggestion, setSuggestion] = useState("");
+
+  const debouncedSuggestion = useCallback(
+    debounce((val) => setSuggestion(val)),
+    []
+  );
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    debouncedSuggestion(e.target.value);
+  };
+
+  useEffect(() => {
+    let isCurrent = true;
+    if (suggestion.length === 0) return;
+    setLoading(true);
+    const queryString =
+      "q=" +
+      encodeURIComponent(`repo:facebook/react ${suggestion} in:title in:body`);
+    axios
+      .get(`https://api.github.com/search/issues?${queryString}`)
+      .then((res) => {
+        if (isCurrent) {
+          setOptions(res.data.items.map((item) => item.title));
+          setLoading(false);
+        }
+
+        return () => {
+          isCurrent = false;
+        };
+      });
+  }, [loading, suggestion]);
+
+  useEffect(() => {
+    if (!open) setOptions([]);
+  }, [open]);
+
+  return (
+    <Container>
+      <Box>
+        <Autocomplete
+          open={open}
+          onOpen={() => {
+            setOpen(true);
+          }}
+          onClose={() => {
+            setOpen(false);
+          }}
+          loading={loading}
+          options={options}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Search Issues"
+              value={searchValue}
+              onChange={handleChange}
+              InputProps={{
+                ...params.InputProps,
+                endAdornment: (
+                  <>
+                    {loading ? (
+                      <CircularProgress color="inherit" size={20} />
+                    ) : null}
+                    {params.InputProps.endAdornment}
+                  </>
+                ),
+              }}
+            />
+          )}
+        />
+      </Box>
+      <Box>Search Contents</Box>
+    </Container>
+  );
 }
 
 export default App;
